7d71cc59557c0d807ab7ada908563127ca30caeb
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2022-07-18 07:37:12 +0600

    Added Profile page

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,35 +1,31 @@
-import styles from '../styles/EventCard.module.css'
-import Link from 'next/link'
-import {Card} from 'react-bootstrap';
+import styles from "../styles/EventCard.module.css";
+import Link from "next/link";
+import { Card } from "react-bootstrap";
 
-const EventCard = ({event}) => {
-    return ( 
+const EventCard = ({ event }) => {
+    return (
+        <Card className={"bg-dark text-white " + styles.eventCard}>
+            <Card.Img
+                src={event.image}
+                alt="Card image"
+                className={styles.eventBanner}
+            />
 
-        <>
+            <Card.ImgOverlay className={styles.overlay}>
+                <Card.Text className={styles.date}> {event.date} </Card.Text>
 
-            <Card className={"bg-dark text-white " + styles.eventCard}
-            >
+                <Link href={"/event/" + event.url}>
+                    <Card.Title className={styles.title}>
+                        {event.title}
+                    </Card.Title>
+                </Link>
 
-                <Card.Img src={event.image} alt="Card image" className={styles.eventBanner} />
-
-                <Card.ImgOverlay className={styles.overlay}>
-                    <Card.Text className={styles.date}> {event.date} </Card.Text>
-                    
-                    <Link href={"/event/" + event.url}>
-                    <Card.Title className={styles.title}>{event.title}</Card.Title>
-                    </Link>
-                    
-                    
-                    <Card.Text className={styles.description}>
-                        {event.description}
-                    </Card.Text>
-                </Card.ImgOverlay>
-            
-            </Card>
-
-        </>
-
+                <Card.Text className={styles.description}>
+                    {event.description}
+                </Card.Text>
+            </Card.ImgOverlay>
+        </Card>
     );
-}
- 
+};
+
 export default EventCard;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import EventCard from '../components/EventCard'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import EventCard from "../components/EventCard";
 import FormTitle from "../components/Form/FormTitle";
-import { useRouter} from 'next/router'
-import {useState, useEffect} from 'react'
-
-
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { Flex, Stack, Center, Box, useColorModeValue, Heading } from "@chakra-ui/react";
 
 export async function getServerSideProps() {
-
-  let isAuth = null
+  let isAuth = null;
 
   // if(!isAuth){
 
@@ -23,35 +21,34 @@ export async function getServerSideProps() {
   // }
 
   return {
-    props: {  }
-  }
+    props: {},
+  };
 }
 
-
-
 export default function Home() {
-
   const router = useRouter();
 
   let events = [
     {
-      'id': 1,
-      'image': 'event1.jpg',
-      'title': 'Chicago Art Exhibition 2022',
-      'date': 'Dec 12,2021',
-      'url': '1',
-      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+      id: 1,
+      image: "event1.jpg",
+      title: "Chicago Art Exhibition 2022",
+      date: "Dec 12,2021",
+      url: "1",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
 
     {
-      'id': 2,
-      'image': 'event2.jpg',
-      'title': 'Chicago Art Exhibition 2022',
-      'date': 'Dec 12,2021',
-      'url': '2',
-      'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
-    }
-  ]
+      id: 2,
+      image: "event2.jpg",
+      title: "Chicago Art Exhibition 2022",
+      date: "Dec 12,2021",
+      url: "2",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+  ];
 
   // useEffect(()=>{
   //     let isAuth = localStorage.getItem('user')
@@ -61,12 +58,38 @@ export default function Home() {
   //     }
   // });
 
-
   return (
-      <div className="page_style">
-        <FormTitle title="Newsfeed" />
-        <EventCard event={events[0]}/>
-        <EventCard event={events[1]}/>
-      </div>
-  )
+    <Center py={6}>
+      <Box
+        maxW={"90%"}
+        w={"full"}
+        rounded={"md"}
+        overflow={"hidden"}
+      >
+        <Box p={0.1}>
+          <Stack spacing={0} align={"center"}>
+            <Heading fontSize={"3xl"} fontWeight={600} fontFamily={"body"}>
+              Newsfeed
+            </Heading>
+          </Stack>
+
+          <Center px={10}>
+            <Box
+              maxW="md"
+              w={"full"}
+              rounded={"md"}
+              overflow={"hidden"}
+            >
+
+            <Stack spacing={0} align={'center'}>
+              <EventCard event={events[0]} />
+              <EventCard event={events[1]} />
+            </Stack>
+            </Box>
+          </Center>
+
+        </Box>
+      </Box>
+    </Center>
+  );
 }
