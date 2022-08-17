d21d908c18aa4903f86748a85d06b079db9d37a8
Purbasha Nishat <1705067@ugrad.cse.buet.ac.bd>
2022-08-18 04:54:21 +0600

    Styling added in search.js

--- pages/search.js
+++ pages/search.js
@@ -2,18 +2,53 @@ import { useState } from 'react';
 import Router from "next/router";
 import FormTitle from "../components/Form/FormTitle";
 
+import {Stack, Image, Flex, Avatar, Box, Text, Spacer, Button, Textarea, Center, Input, Container, VStack} from "@chakra-ui/react";
+import { postData } from '../services/HttpService';
+import {getData_Local, storeData_Local} from '../services/StorageService';
 
+const Search = () => {
 
-const search = () => {
+    const [search, setSearch] = useState('');
+
+    const handleSearchChange = () => {
+        console.log("Value: ",search);
+        
+        // const eventUrl = '';
+        // const payload = {
+        //     evenName: search
+        // }
+
+        // postData(eventUrl, payload)
+        // .then((data) => {
+        //     console.log("Response data:", data);
+        //     storeData_Local("token", data.token);
+        // }).catch((err) => {
+        //     console.log(err);
+        // });
+    }
 
     return (
 
         <div className="page_style">
             <FormTitle title="Search" />
+
+            <VStack>
+                <Container maxW='2xl' >
+                    <Flex>
+                        <Input type='text' variant='filled' placeholder='Search...' value={search} onChange={(e) => {setSearch(e.target.value)}} />
+                        <Spacer/>
+                        <Button colorScheme='teal' variant='solid' onClick={() => handleSearchChange()}>
+                            Search
+                        </Button>
+                    </Flex>
+                </Container>
+            </VStack>
+
+            
         
         </div>
 
     );
 }
 
-export default search;
+export default Search;
