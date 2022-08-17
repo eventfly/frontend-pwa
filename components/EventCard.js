1887a3591e93dbfb7413ba3dbb413e3aa5b65118
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2022-08-17 18:43:48 +0600

    Previous Cards from old commit added

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,50 +1,31 @@
-import styles from '../styles/EventCard.module.css'
-// import Link from 'next/link'
-import {Card} from 'react-bootstrap';
-import {Image,Link, Flex, Avatar, Box, Text,Stack, Spacer, Button, Textarea} from "@chakra-ui/react";
+import styles from "../styles/EventCard.module.css";
+import Link from "next/link";
+import { Card } from "react-bootstrap";
 
-const EventCard = ({event}) => {
-    return ( 
+const EventCard = ({ event }) => {
+    return (
+        <Card className={"bg-dark text-white " + styles.eventCard}>
+            <Card.Img
+                src={event.image}
+                alt="Card image"
+                className={styles.eventBanner}
+            />
 
-        <>
+            <Card.ImgOverlay className={styles.overlay}>
+                <Card.Text className={styles.date}> {event.date} </Card.Text>
 
-            {/* <Card className={"bg-dark text-white " + styles.eventCard} >
+                <Link href={"/event/" + event.url}>
+                    <Card.Title className={styles.title}>
+                        {event.title}
+                    </Card.Title>
+                </Link>
 
-                <Card.Img src={event.image} alt="Card image" className={styles.eventBanner} /> 
-
-                 <Card.ImgOverlay className={styles.overlay}>
-                    <Card.Text className={styles.date}> {event.date} </Card.Text>
-                    
-                    <Link href={"/event/" + event.url}>
-                    <Card.Title className={styles.title}>{event.title}</Card.Title>
-                    </Link>
-                    
-                    
-                    <Card.Text className={styles.description}>
-                        {event.description}
-                    </Card.Text>
-                </Card.ImgOverlay>
-            </Card> */}
-
-            <Box className={"bg-dark text-white " + styles.eventCard} backgroundImage={event.image}>
-                <Flex className={styles.overlay}>
-                        <Text>
-                            {event.date}
-                        </Text>
-                        <Link className={styles.title} color='white' href={"/event/" + event.url} fontSize="xx-large" fontWeight='bold'>
-                            {event.title}
-                        </Link>
-                        <Text fontSize="large">
-                            {event.description}
-                        </Text>
-                    
-                </Flex>
-            
-            </Box>
-
-        </>
-
+                <Card.Text className={styles.description}>
+                    {event.description}
+                </Card.Text>
+            </Card.ImgOverlay>
+        </Card>
     );
-}
- 
+};
+
 export default EventCard;
---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import UserProfileCard from "../components/UserProfileCard";
+
+
+export default function Profile() {
+
+    
+    return (
+        <UserProfileCard/>
+    )
+
+}
